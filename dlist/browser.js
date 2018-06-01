2c3be9be1cf7b3eea2ba8b495eb8ad840ef87f49
谦谦 <wangwenxiao03@163.com>
2018-06-01 17:21:06 +0800

    检测当前浏览器为什么浏览器，并检测其版本

---
+++ dlist/browser.js
@@ -0,0 +1,10 @@
+function browser(){
+			var _user = win.navigator.userAgent,_isAlert = false;
+
+			var _browserName = _user.match(/(Chrome|MSIE|Firefox|rv|Version)[:|\/|\s]{1}([\w\.]+)/);
+
+			if(_browserName)
+			{ 
+				  console.log("浏览器为"+_browserName[1]+",版本为"+_browserName[2]);
+			}
+}
